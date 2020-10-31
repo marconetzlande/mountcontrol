8eb87d119eddf233f6bdefd7032250d43aac29e9
marconetzlande <37881516+marconetzlande@users.noreply.github.com>
2020-10-31 15:33:20 +0100

    added Serial communication

--- SimpleRA_DEC_Guider-StepperOnly/SimpleRA_DEC_Guider-StepperOnly.ino
+++ SimpleRA_DEC_Guider-StepperOnly/SimpleRA_DEC_Guider-StepperOnly.ino
@@ -30,6 +30,7 @@ void loop () {
        DEC_Stepper.disable();
     }
   }
+  
   static boolean last_r = LOW;
   boolean r = !digitalRead(7);
   if (r && !last_r) {
@@ -45,4 +46,67 @@ void loop () {
   
   if (RA_Stepper.actionImminent()) RA_Stepper.nextAction();
   if (DEC_Stepper.actionImminent()) DEC_Stepper.nextAction();
+
+  if (Serial.available() > 0) {
+      char command = Serial.read();
+      switch (command) {
+        /*
+        case 'G':
+          Serial.println("Starting RA guider");
+          stopGoto();
+          startGuiding();
+        break;
+        case 'D': if (true) {
+            volatile String s;
+            s = Serial.readStringUntil('#');
+            ra_divisor = s.toInt();
+            delay(250);
+          }
+        break;          
+        */
+        case 'O': if (true) {
+            volatile String s;
+            s = Serial.readStringUntil(',');
+            volatile long r = s.toInt();
+            s = Serial.readStringUntil('#');
+            volatile long d = s.toInt();
+            Serial.print("Movin: dRA:");
+            Serial.print(r);
+            Serial.print(", dDEC:");
+            Serial.print(d);
+            Serial.println();
+            delay(250);
+            RA_Stepper.move(r);
+            DEC_Stepper.move(d);
+          }
+        break;
+        case 'Z':
+          Serial.println("Going to home position");
+          Serial.println(-RA_Stepper.getSteps());
+          Serial.println(-DEC_Stepper.getSteps());
+          delay(100);
+          RA_Stepper.move(-RA_Stepper.getSteps());
+          DEC_Stepper.move(-DEC_Stepper.getSteps());
+          break;
+        /*
+        case 'S':
+          Serial.println("Steps to do");
+          Serial.println(goto_steps[stRA]);
+          Serial.println(goto_steps[stDEC]);
+          delay(250);
+        break;
+        */
+        case 'P':
+          Serial.println("Current position");
+          Serial.println(RA_Stepper.getSteps());
+          Serial.println(DEC_Stepper.getSteps());
+          delay(250);
+        break;
+        case 'X':
+          Serial.println("Stopping everything");
+          delay(250);
+        break;
+      }
+      while (Serial.available()) Serial.read();
+   }
 }
